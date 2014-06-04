25b94cc560d48c4b3c4e5e0c3f04fab6f1467a98
Munkhbaatar <gmunkhbaatarmn@gmail.com>
2014-06-05 06:11:52 +0800

    problemset filled with data

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
-from natrix import app, route, data, _
+from natrix import app, route, data, json
+from magics import cf_get_active_users, tc_get_active_users
+from models import Problem
 
 
-app.config["context"] = lambda x: {
-    "codeforces": data.fetch("Rating:codeforces", []),
-    "topcoder": data.fetch("Rating:topcoder", []),
-}
+def context(self):
+    return {
+        "codeforces": data.fetch("Rating:codeforces", []),
+        "topcoder": data.fetch("Rating:topcoder", []),
+    }
 
 
 # --- Todo ---
 
 @route("/problemset")
-def problemset(x, id="1"):
-    x.response("OK")
+def problemset_index(x, id="1"):
+    problems = Problem.all().order("-contest_id").\
+        order("-problem_id").fetch(100)
+
+    count = Problem.all().count()
+
+    x.render("problemset-index.html", problems=problems,
+             count=count)
 
 
 @route("/problemset/page/(\d+)")
-def problemset_paged(x, id="1"):
+def problemset(x, id="1"):
+    problems = Problem.all().order("-contest_id").\
+        order("-problem_id").fetch(100)
+
+    count = Problem.all().count()
+
+    x.render("problemset-index.html", problems=problems,
+             count=count)
+
+
+@route("/migrate")
+def migrate(x):
+    d = json.loads(open("data.json").read())
+
+    for p in d:
+        pr = Problem(contest_id=int(p[0]),
+                     problem_id=p[1],
+                     title=p[2],
+                     content=p[4],
+                     markdown=p[3])
+        pr.save()
+
     x.response("OK")
+
 
 
 # === Done ===
@@ -33,6 +64,9 @@ def ratings(x):
 
 @route("/-/ratings")
 def cron_ratings(x):
-    data.write("Rating:codeforces", _.cf_get_active_users())
-    data.write("Rating:topcoder", _.tc_get_active_users())
+    data.write("Rating:codeforces", cf_get_active_users())
+    data.write("Rating:topcoder", tc_get_active_users())
     x.response("OK")
+
+
+app.config["context"] = context
