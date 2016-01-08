229927b65b250ce42e8654b3ea623f45c9967515
AMAR <sbamaraa@gmail.com>
2016-01-08 17:23:22 +0800

    fix test_problem [ parse_test.py ]

--- parse_test.py
+++ parse_test.py
@@ -29,8 +29,8 @@ def test_problem():
     eq(p["output"], "standard output")
     eq(p["memory"], "256 megabytes")
     eq(p["time"], "1 second")
-    eq(p["tests"][0], ("1 3 2 1 5 10\n0 10", "30"))
-    eq(p["tests"][1], ("2 8 4 2 5 10\n20 30\n50 100", "570"))
+    eq(p["tests"][0], ("1 3 2 1 5 10\n0 10\n", "30"))
+    eq(p["tests"][1], ("2 8 4 2 5 10\n20 30\n50 100\n", "570"))
 
 
 @attr("focus")
